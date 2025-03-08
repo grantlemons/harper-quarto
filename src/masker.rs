956c985fb2db09533b2ea3cf7efec8538a56e96f
Grant Lemons <grantlemons@aol.com>
2025-03-08 13:05:40 -0700

    implement masking out header

--- src/masker.rs
+++ src/masker.rs
@@ -1,16 +1,36 @@
-use harper_core::{CharStringExt, Mask, Masker, Span};
+use harper_core::{Mask, Masker, Span};
 use itertools::Itertools;
 
 pub struct QuartoMasker;
 
 fn isolate_document(source: &[char]) -> Option<Vec<Span>> {
-    let doc = todo!();
+    struct SourceLine {
+        dashes: bool,
+        span: Span,
+    }
 
-    Some(
-        doc.split(|(_, c)| **c == '\n')
-            .filter_map(|l| Some(Span::new(l.first()?.0, l.last()?.0 + 1)))
-            .collect_vec(),
-    )
+    let lines = source
+        .into_iter()
+        .enumerate()
+        .collect_vec()
+        .split(|(_, c)| **c == '\n')
+        // removes empty lines as well
+        .filter_map(|l| {
+            Some(SourceLine {
+                dashes: l.iter().map(|(_, c)| *c).all(|&c| c == '-'),
+                span: Span::new(l.first()?.0, l.last()?.0 + 1),
+            })
+        })
+        .collect_vec();
+
+    let first_line_dashes = lines.first()?.dashes;
+    let res = if first_line_dashes {
+        lines.splitn(3, |l| l.dashes).nth(2)?.iter()
+    } else {
+        lines.iter()
+    };
+
+    Some(res.map(|l| l.span).collect())
 }
 
 impl Masker for QuartoMasker {
@@ -36,18 +56,78 @@ mod tests {
     use super::QuartoMasker;
 
     #[test]
-    fn parse_no_header() {
+    fn mask_no_header() {
         let src = "# Line
+---
+line
+# Line
 ---
 line
 line
 ";
+        let expected = src.trim_end().chars().collect_vec();
         let src = src.chars().collect_vec();
 
         let masker = QuartoMasker;
         let mask = masker.create_mask(&src);
 
         let allowed = mask.iter_allowed(&src).next().expect("No allowed blocks").1;
-        assert_eq!(allowed, &src);
+        assert_eq!(allowed, expected);
+    }
+
+    #[test]
+    fn mask_newline_before_header() {
+        let src = "
+---
+Header contents
+---
+line
+line
+";
+        let expected = src[25..].trim_end().chars().collect_vec();
+        let src = src.chars().collect_vec();
+
+        let masker = QuartoMasker;
+        let mask = masker.create_mask(&src);
+
+        let allowed = mask.iter_allowed(&src).next().expect("No allowed blocks").1;
+        assert_eq!(allowed, expected);
+    }
+
+    #[test]
+    fn mask_header() {
+        let src = "---
+Header contents
+---
+line
+line
+";
+        let expected = src[24..].trim_end().chars().collect_vec();
+        let src = src.chars().collect_vec();
+
+        let masker = QuartoMasker;
+        let mask = masker.create_mask(&src);
+
+        let allowed = mask.iter_allowed(&src).next().expect("No allowed blocks").1;
+        assert_eq!(allowed, expected);
+    }
+
+    #[test]
+    fn mask_false_header() {
+        let src = "This document contains text!
+---
+False header contents
+---
+line
+line
+";
+        let expected = src.trim_end().chars().collect_vec();
+        let src = src.chars().collect_vec();
+
+        let masker = QuartoMasker;
+        let mask = masker.create_mask(&src);
+
+        let allowed = mask.iter_allowed(&src).next().expect("No allowed blocks").1;
+        assert_eq!(allowed, expected);
     }
 }
